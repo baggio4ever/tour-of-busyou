31fd8ee2ca6355ab90ddf3c3f4abc64dc3c5b996
hirayama <hirayama@bp.horizon.co.jp>
2017-03-16 15:16:55 +0900

    武将を *ngFor で表示するようにした

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,16 @@ import { Component } from '@angular/core';
 
 import { Busyou } from './busyou';
 
+const BUSYOUS:Busyou[] = [
+  { id:2, name:'織田信長'},
+  { id:3, name:'豊臣秀吉'},
+  { id:4, name:'徳川家康'},
+  { id:5, name:'武田信玄'},
+  { id:6, name:'北条氏康'},
+  { id:7, name:'今川義元'},
+  { id:8, name:'浅井長政'},
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,4 +23,6 @@ export class AppComponent {
       id:1,
       name:'上杉謙信'
   };
+
+  busyous = BUSYOUS;
 }
