82e5bb2d2c5954b23365f6c6a256f19071a81898
baggio4ever <baggio4ever@gmail.com>
2017-03-17 23:16:06 +0900

    Routing章完了

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { BusyouService } from './busyou.service';
 
 
 @Component({
+  moduleId: module.id,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
