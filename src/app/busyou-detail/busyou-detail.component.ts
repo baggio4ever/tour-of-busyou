d20671e09ef00011109a8a5ee1e5ea66a0f17307
baggio4ever <baggio4ever@gmail.com>
2017-03-28 22:18:07 +0900

    DetailComponent修正中

--- src/app/busyou-detail/busyou-detail.component.ts
+++ src/app/busyou-detail/busyou-detail.component.ts
@@ -42,4 +42,32 @@ export class BusyouDetailComponent implements OnInit {
   get diagnostic() {
     return JSON.stringify(this.busyou);
   }
+
+  public alerts: any = [];
+ 
+  public ddd(): void {
+    let val = Math.random();
+    if(val>0.6){
+        this.alerts.push({
+          type: 'info',
+          msg: `オッス！オラ ${this.busyou.name}！！`,
+          timeout: 5000
+        });
+
+    } else if(val>0.3) {
+        this.alerts.push({
+          type: 'success',
+          msg: `はい！私  ${this.busyou.name} でございます！！`,
+          timeout: 5000
+        });
+
+    } else {
+        this.alerts.push({
+          type: 'danger',
+          msg: `イェェェェイ！ ${this.busyou.name}！！ジャァァァスティス！！！`,
+          timeout: 5000
+        });
+
+    }
+  }
 }
