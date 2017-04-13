64f1e34fca88e7a78bf5d5b7620ecec384ffe03e
hirayama <hirayama@bp.horizon.co.jp>
2017-04-13 14:40:48 +0900

    bootstrap周りを試し中

--- src/app/busyou.ts
+++ src/app/busyou.ts
@@ -2,8 +2,8 @@ export class Busyou {
     id: number;
     name: string; // 武将名
     kana: string; // ふりがな
-    param_buryoku: number; // 武力
+    param_buryoku: number; // 武勇
     param_seiji: number; // 政治
-    param_chibou: number; // 知力
+    param_chibou: number; // 知略
     img: string;
 }
